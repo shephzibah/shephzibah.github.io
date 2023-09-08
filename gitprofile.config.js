da3dadf49c85a4473ca1a3cb2d3614dd0fae7394
Hephzibah Saidu <98137690+shephzibah@users.noreply.github.com>
2023-09-07 21:16:23 -0700

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -1,14 +1,14 @@
 // gitprofile.config.js
 
 const config = {
-  github: {
+ /* github: {
     username: 'shephzibah', // Your GitHub org/user name. (Required)
     sortBy: 'stars', // stars | updated
     limit: 10, // How many projects to display.
     exclude: {
       forks: false, // Forked projects will not be displayed if set to true.
       projects: [], // These projects will not be displayed. example: ['my-project1', 'my-project2']
-    },
+    },*/
   },
   social: {
     linkedin: 'hephzibah-saidu',
