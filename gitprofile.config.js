ea9f3e886cf3399735434e512d1c463009b3a1bb
Hephzibah Saidu <98137690+shephzibah@users.noreply.github.com>
2023-10-21 13:45:35 -0700

    Update gitprofile.config.js
    changed resume url

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -30,7 +30,7 @@ const config = {
   },
   resume: {
     fileUrl:
-      'https://github.com/shephzibah/shephzibah.github.io/blob/main/Software_Engineer_HS.pdf', // Empty fileUrl will hide the `Download Resume` button.
+      'Software_Engineer_HS.pdf', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
     'Java', 
